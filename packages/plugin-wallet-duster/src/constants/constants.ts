61ad9c5c0bb79925e49c9f26eb5456793e9dd844
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2025-06-23 21:52:02 +0800

    Add transfer function to ERC20_ABI in constants.ts
    Implemented the transfer function in the ERC20_ABI to enable token transfers, enhancing the contract's functionality for users.

--- packages/plugin-wallet-duster/src/constants/constants.ts
+++ packages/plugin-wallet-duster/src/constants/constants.ts
@@ -75,6 +75,29 @@ export const ERC20_ABI = [
         stateMutability: "view",
         type: "function",
     },
+    {
+        constant: false,
+        inputs: [
+            {
+                name: "_to",
+                type: "address",
+            },
+            {
+                name: "_value",
+                type: "uint256",
+            },
+        ],
+        name: "transfer",
+        outputs: [
+            {
+                name: "",
+                type: "bool",
+            },
+        ],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
 ];
 
 export const mockGetQuoteResponse: GetQuoteResponse = {
