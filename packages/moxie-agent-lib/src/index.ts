08bed004017a23b90d68f6ec5148f44ca1882e8a
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2025-07-23 22:47:08 +0800

    Refactor tokenDetails service by removing the deprecated getTrendingTokenDetails function and simplifying the getTokenDetails function. Update exports in index.ts for clarity and organization.

--- packages/moxie-agent-lib/src/index.ts
+++ packages/moxie-agent-lib/src/index.ts
@@ -8,12 +8,11 @@ export { ftaService };
 
 export { walletService };
 
-
 export { RedisClient } from "./services/RedisClient";
 
 export { MoxieAgentDBAdapter } from "./services/MoxieAgentDBAdapter";
 
-export { getTokenDetails, getTrendingTokenDetails } from "./services/tokenDetails";
+export { getTokenDetails } from "./services/tokenDetails";
 
 export { getERC20TokenSymbol } from "./services/tokenSymbol";
 
@@ -67,4 +66,9 @@ export {
 
 export { deleteLimitOrders } from "./services/cowService";
 
-export { formatUserMention, formatGroupMention, formatTokenMention, fetchWithRetries } from "./utils";
+export {
+    formatUserMention,
+    formatGroupMention,
+    formatTokenMention,
+    fetchWithRetries,
+} from "./utils";
