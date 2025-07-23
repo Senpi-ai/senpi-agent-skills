059f0288e51939404b44d989ca155e9446b2eb62
Yoseph Kurnia S. <48710170+YosephKS@users.noreply.github.com>
2025-07-23 23:02:26 +0800

    Update swapSummaryAction.ts

--- packages/plugin-moxie-social-alpha/src/actions/swapSummaryAction.ts
+++ packages/plugin-moxie-social-alpha/src/actions/swapSummaryAction.ts
@@ -18,7 +18,7 @@ import * as templates from "../templates";
 import { fetchSwapData } from "../utils";
 import { TOP_CREATORS_COUNT } from "../config";
 import { getMoxieIdsFromMessage, streamTextByLines, handleIneligibleMoxieUsers } from "./utils";
-import { getTokenDetails, getTrendingTokenDetails, MoxieAgentDBAdapter, MoxieUser, moxieUserService } from "@moxie-protocol/moxie-agent-lib";
+import { getTokenDetails, MoxieAgentDBAdapter, MoxieUser, moxieUserService } from "@moxie-protocol/moxie-agent-lib";
 export const tokenSwapSummary: Action = {
     name: "TRENDING_TOKENS",
     suppressInitialMessage: true,
@@ -302,10 +302,6 @@ async function swapSummaryHandler(
     elizaLogger.debug(`tokenAddresses: ${tokenAddresses}`);
 
     let tokenDetails = await getTokenDetails(tokenAddresses);
-
-    if (tokenType === "NON_CREATOR_COIN") {
-        tokenDetails = await getTrendingTokenDetails(tokenAddresses);
-    }
 
     elizaLogger.debug(`tokenDetails: ${JSON.stringify(tokenDetails)}`)
 
